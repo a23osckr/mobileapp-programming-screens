943e247f2d9dacf491d41bd1ca3b51d65f4374f1
AOkay12 <oscarkruit@live.se>
2024-04-15 14:33:07 +0200

    SecondActivity tar emot datan skickad från Main

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -10,5 +10,11 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String status = extras.getString("status");
+            int errors = extras.getInt("errors");
+        }
     }
 }
