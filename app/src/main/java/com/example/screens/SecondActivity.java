54be3e97604106b67b19491ed1d138763143bec0
AOkay12 <oscarkruit@live.se>
2024-04-15 14:42:06 +0200

    Lade till den importerade datan i 2st textviews

--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -1,6 +1,7 @@
 package com.example.screens;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +16,12 @@ public class SecondActivity extends AppCompatActivity {
         if (extras != null) {
             String status = extras.getString("status");
             int errors = extras.getInt("errors");
+
+            TextView errorsTextView = findViewById(R.id.errorsTextView);
+            TextView statusTextView = findViewById(R.id.statusTextView);
+
+            errorsTextView.append(status);
+            statusTextView.append(String.valueOf(errors));
         }
     }
 }
